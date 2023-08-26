2c9bb49f34f479a88b779c07b886504b81276e9c
Sohan jagadeesh <sjag0009@student.monash.edu>
2023-08-26 21:47:23 +1000

    finished Landing Page. Finalizing colors.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import styles from "../style";
+import { discount, robot } from "../assets";
+import Button from "./Button";
+
+const Hero = () => {
+  return (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            From Rupees to 
+            <span className="text-gradient"> Dollars</span>
+          </h1>
+        </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          Your <span className="text-gradient"> Financial Compass</span>
+          <br className="sm:block hidden" />{" "} <span>in Australia</span>{" "}
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Empowering India's bright students in Australia with the tools they need to thrive.
+        </p>
+        <Button styles={`mt-10`} />
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/pages/dollar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "../components/Navbar";
+
+
+const dollar = () => (
+  <div className="bg-primary">
+    <Navbar/>
+    <div>Your Dollar's Worth</div>
+  </div>
+);
+
+
+export default dollar
