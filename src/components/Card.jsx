3093334c075c46f185957090d1cd09699cdd1467
Sohan jagadeesh <sjag0009@student.monash.edu>
2023-09-01 02:07:29 +1000

    Feature 1 completely done, all functionalities

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import React from 'react'
 
-const CategoryCard = ({ title, days }) => {
+const CategoryCard = ({ title, days, description }) => {
     return (
       <div className="category-card bg-blue-gradient p-9 rounded-md shadow-md">
         <h3 className="text-xl text-center font-semibold capitalize text-black">{title}</h3>
         <p className="mt-2 text-lg text-bold text-center font-bold">{days} days</p>
         <br/>
-        <p>If it's the food card, add a toggle or dropdown hereIf it's the food card, add a toggle or dropdown hereIf it's the food card, add a toggle or dropdown here</p>
+        <p className="text-center">{description}</p>
         {/* If it's the food card, add a toggle or dropdown here */}
       </div>
     );
--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -6,12 +6,25 @@ const Output = ({ response }) => {
         return <div>No data received.</div>;
     }
 
-    const itemNames = ["coffee", "housing", "Transport", "Food and non-alcoholic beverages", "Months with entire expenditure", "Utilities"];
+    const itemsData = [
+      { name: "Coffee", description: "Your can afford coffee for the number of days mentioned above" },
+      { name: "Housing", description: "Your can afford to pay rent for the number of days mentioned above" },
+      { name: "Transport", description: "Your can afford transportation for the number of days mentioned above" },
+      { name: "Food and Non-Alcoholic Beverages", description: "Your can afford food for the number of days mentioned above" },
+      { name: "All Expenses Included", description: "You can afford to fulfill all expenditures for the number of days mentioned above." },
+      { name: "Utilities", description: "Your can afford to pay for utilites for the number of days mentioned above" },
+      // ... other items ...
+    ];
+
+    const adjustmentMultipliers = [1, 1.5, 1.2 , 1, 2.2 , 3];
 
     return (
       <div className="output-section mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {response.results.map((value, index) => (
-          <CategoryCard key={index} title={itemNames[index] || `Item ${index + 1}`} days={value} />
+          <CategoryCard key={index}
+          title={itemsData[index].name}
+          days= {Math.round(value * adjustmentMultipliers[index] )}
+          description={itemsData[index].description} />
         ))}
       </div>
     );
