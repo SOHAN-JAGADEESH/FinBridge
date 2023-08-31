266800734620ba76209b90e255ba1f99d7ffcac7
Sohan jagadeesh <sjag0009@student.monash.edu>
2023-09-01 01:17:31 +1000

    Viewport height changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import { Billing, Business, CardDeal, CTA, Footer, Navbar, Hero } from "./compon
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <div style={{minHeight: "100vh"}}>
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
+    {/* This is the main container for Navbar and Hero */}
+    <div className="flex flex-col h-screen overflow-hidden">
+      <div className={`${styles.paddingX} flex flex-grow`}>
+        <div className={`${styles.boxWidth} w-full`}>
           <Navbar />
         </div>
       </div>
-
-      <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
+      <div className={`bg-primary flex flex-grow ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth} w-full`}>
           <Hero />
         </div>
       </div>
     </div>
-    
+
     <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Business />
