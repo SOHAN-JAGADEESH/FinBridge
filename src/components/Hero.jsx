951f717bcf5730829c293612420cad1227c93e8f
Sohan jagadeesh <sjag0009@student.monash.edu>
2023-09-01 01:32:14 +1000

    check 5

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY} `}>
-      <div className={`flex-1 ${styles.flexStart} flex-col max-h-full xl:px-0 sm:px-16 px-6`}>
+    <section id="home" className={`flex md:flex-row max-h-full flex-col ${styles.paddingY} `}>
+      <div className={`flex-1 ${styles.flexStart} flex-col  xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[67px]">
             From Rupees to 
