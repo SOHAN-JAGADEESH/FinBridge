5b1837329e7f203b85f32d83f554beb468c2ca08
Sohan jagadeesh <sjag0009@student.monash.edu>
2023-09-01 01:28:30 +1000

    check 3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY} `}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[67px]">
             From Rupees to 
             <span className="text-gradient"> Dollars</span>
           </h1>
@@ -23,8 +23,8 @@ const Hero = () => {
         <Button styles={`mt-10`} />
       </div>
 
-      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative overflow-hidden`}>
+        <img src={robot} alt="billing" className="w-[100%] max-h-full relative z-[5]" />
       </div>
     </section>
   );
