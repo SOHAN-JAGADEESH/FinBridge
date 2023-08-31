17905caa56cd83c18de32e25145d7ad061ae8dbe
Sohan jagadeesh <sjag0009@student.monash.edu>
2023-09-01 01:23:30 +1000

    check 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import { Billing, Business, CardDeal, CTA, Footer, Navbar, Hero } from "./compon
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     {/* This is the main container for Navbar and Hero */}
-    <div className="flex flex-col h-screen overflow-auto">
+    <div className="flex flex-col h-screen">
       <div className={`${styles.paddingX} flex-shrink-0`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
         </div>
       </div>
-      <div className={`bg-primary flex-grow ${styles.flexStart} overflow-y-auto`}>
+      <div className={`bg-primary flex-grow ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Hero />
         </div>
