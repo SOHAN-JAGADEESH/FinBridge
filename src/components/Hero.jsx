f33693ff1b71eb3f5440e8982e9a217ce61f3ac3
Sohan jagadeesh <sjag0009@student.monash.edu>
2023-09-03 19:34:07 +1000

    check hero section 3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,27 +5,22 @@ import Button from "./Button";
 const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-2`}>
+      <div className={` ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-2`}>
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className=" font-poppins font-semibold text-4xl md:text-5xl lg:text-6xl xl:text-[52px] 2xl:text-[72px] text-white leading-snug md:leading-normal lg:leading-relaxed">
             From <span className="text-gradient">Rupees </span>to 
-            <span className="text-gradient"> Dollars </span>
+            <span className="text-gradient"> Dollars </span> <br/>
             Your <span className="text-gradient"> Financial Compass</span>
             <br className="sm:block hidden" /> <span>in Australia</span>
           </h1>
         </div>
-
-        {/* <h1 className="font-poppins font-semibold text-4xl md:text-5xl lg:text-6xl xl:text-[52px] 2xl:text-[68px] text-white leading-snug md:leading-normal lg:leading-relaxed w-full">
-          Your <span className="text-gradient"> Financial Compass</span>
-          <br className="sm:block hidden" /> <span>in Australia</span>
-        </h1> */}
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Empowering India's bright students in Australia with the tools they need to thrive.
         </p>
         <Button styles={`mt-10`} />
       </div>
 
-      <div className={`flex flex-1 ${styles.flexCenter} md:my-0 my-10 relative overflow-hidden`}>
+      <div className={`flex  ${styles.flexCenter} md:my-0 my-10 relative overflow-hidden`}>
         <img src={robot} alt="billing" className="w-[100%] h-auto max-h-[calc(100vh - 200px)] relative z-[5]" />
       </div>
     </section>
