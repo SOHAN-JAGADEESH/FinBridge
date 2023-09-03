9b99392bcc92c63c7bd53f94723d9a13c95f2ea2
Sohan jagadeesh <sjag0009@student.monash.edu>
2023-09-03 18:08:38 +1000

    Hero section text scaling optimization

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,18 +4,18 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY} `}>
-      <div className={`flex-1 ${styles.flexStart} flex-col  xl:px-0 sm:px-16 px-6`}>
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[67px]">
+          <h1 className="flex-1 font-poppins font-semibold text-4xl md:text-5xl lg:text-6xl xl:text-[52px] 2xl:text-[72px] text-white leading-snug md:leading-normal lg:leading-relaxed">
             From Rupees to 
             <span className="text-gradient"> Dollars</span>
           </h1>
         </div>
 
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+        <h1 className="font-poppins font-semibold text-4xl md:text-5xl lg:text-6xl xl:text-[52px] 2xl:text-[68px] text-white leading-snug md:leading-normal lg:leading-relaxed w-full">
           Your <span className="text-gradient"> Financial Compass</span>
-          <br className="sm:block hidden" />{" "} <span>in Australia</span>{" "}
+          <br className="sm:block hidden" /> <span>in Australia</span>
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Empowering India's bright students in Australia with the tools they need to thrive.
